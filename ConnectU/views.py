d645b23937e96899150db8187023ec454eda4e49
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2025-11-22 18:50:43 -0600

    Profile Page (Initial) Commit
    Starting setup for Profile Page Feature.

--- ConnectU/views.py
+++ ConnectU/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 from django.http import JsonResponse
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views import View
+
+class LoginView(View):
+    def get(self,request):
+        return render(request,'login.html')
+
+    def post(self,request):
+        return render(request,'login.html')
+    
 
 def test_api(request):
     data = {"message": "Frontend and Backend are connected!"}
