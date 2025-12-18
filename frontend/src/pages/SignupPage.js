a9b6d956b755e4bc3d6bea860b857b27259249c6
Russell-Y <150970166+Russell-Y@users.noreply.github.com>
2025-12-17 18:21:12 -0600

    Sprint 3 Commit (Russell Yuan)
    (Russell Yuan) Added Sprint 3 features: ability to add Profile Pictures, Basic Search Bar

--- frontend/src/pages/SignupPage.js
+++ frontend/src/pages/SignupPage.js
@@ -4,14 +4,16 @@ function SignupPage() {
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [imageSrc, setImageSrc] = useState('https://static.vecteezy.com/system/resources/previews/036/280/651/original/default-avatar-profile-icon-social-media-user-image-gray-avatar-icon-blank-profile-silhouette-illustration-vector.jpg');
 
   const handleSignup = (e) => {
     e.preventDefault();
-    alert(`Signing up:\nUsername: ${username}\nEmail: ${email}\nPassword: ${password}`);
+    alert(`Signing up:\nUsername: ${username}\nEmail: ${email}\nPassword: ${password}\nAvatar: ${imageSrc}`);
   };
 
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <img src={imgSrc} alt={"Avatar"} />
       <h2>Create an Account</h2>
       <form onSubmit={handleSignup}>
         <input
@@ -36,6 +38,13 @@ function SignupPage() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           style={{ margin: "10px", padding: "8px" }}
+        />
+        <br />
+        <input
+          type="imageSrc"
+          placeholder="ImageSrc"
+          value={imageSrc}
+          onChange={(e) => setImageSrc(e.target.value)}
         />
         <br />
         <button type="submit" style={{ padding: "10px 20px" }}>Sign Up</button>
